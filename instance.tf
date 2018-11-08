629764d99262f59d28ec392c3d7cf69774439ae0
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2018-11-07 20:21:00 -0500

    Starts to provision instance

--- instance.tf
+++ instance.tf
@@ -8,11 +8,18 @@ resource "aws_key_pair" "auth" {
 }
 
 resource "aws_security_group" "instance" {
-  name = "Allow SSH and all outgoing"
+  name = "Allow SSH, web traffic, and outgoing"
 
   ingress {
     from_port = 22
     to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -34,6 +41,17 @@ resource "aws_instance" "instance" {
         type = "ssh"
         user = "ec2-user"
         private_key = "${file("~/.ssh/id_rsa")}"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo yum -y install aspell aspell-en httpd24 mysql mysql-server php56 php56-cli php56-gd php56-intl php56-mbstring php56-mysqlnd php56-opcache php56-pdo php56-soap php56-xml php56-xmlrpc php56-pspell",
+      "sudo /sbin/chkconfig httpd on",
+      "sudo /sbin/chkconfig mysqld on",
+      "sudo /sbin/service httpd start",
+#      "sudo /sbin/service mysqld start",
+      "sudo echo 'THIS BE MOODLE' >> /var/www/html/moodle.html",
+    ]
   }
 }
 
